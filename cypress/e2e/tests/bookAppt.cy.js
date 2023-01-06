51e5f3f8ed7f3402fefc96aaa9c96c5c84234a74
sd576 <sd576@btinternet.com>
2023-01-06 13:18:28 +0000

    commented out cy.reuest method in bookAppt.cy.js tes t file

--- cypress/e2e/tests/bookAppt.cy.js
+++ cypress/e2e/tests/bookAppt.cy.js
@@ -1,23 +1,9 @@
 import appointmentPage from '../pages/appointmentPage';
 
 describe('book appointment', () => {
-  beforeEach(() => {
-    cy.request({
-      method: 'POST',
-      url: '/profile.php#login',
-      form: true,
-      body: {
-        user: {
-          username: 'John Doe',
-          password: 'ThisIsNotAPassword',
-        },
-      },
-    });
-    // to prove we have a session
-    cy.getCookie('cypress-session-cookie').should('exist');
-
-    cy.visit('/');
-  });
+  //   beforeEach(() => {
+  //     cy.request();
+  //   });
 
   it('Appointment 1', () => {
     // const appt = new appointmentPage();
